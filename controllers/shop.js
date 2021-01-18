274bbb243960f7b65eb399bbb0518562c79c4bd4
Arthur Melkonyan <arthur@artmelkon.me>
2021-01-18 03:36:41 +0200

    update

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,6 +4,36 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
+const ITEMS_PER_PAGE = 1;
+
+exports.getIndex = (req, res, next) => {
+  const page =+ req.query.page || 1;
+  let totalItems;
+
+  Product.find()
+    .countDocuments()
+    .then(numberProducts => {
+      totalItems = numberProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
+    .then( products => {
+      res.render('shop/index', {
+        prods: products,
+        docTitle: 'Shop',
+        path: '/',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPrevPage: page > 1,
+        nextPage: page + 1,
+        prevPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
+      });
+    })
+    .catch( err => console.error(err));
+}
+
 exports.getProducts = (req, res, next) => {
   // special rendering method from express pointing to file name
   Product.find()
@@ -33,18 +63,6 @@ exports.getProduct = (req, res, next) => {
     })
   })
   .catch(err => console.log(err));
-}
-
-exports.getIndex = (req, res, next) => {
-  Product.find()
-    .then( products => {
-      res.render('shop/index', {
-        prods: products,
-        docTitle: 'Shop',
-        path: '/'
-      });
-    })
-    .catch( err => console.error(err));
 }
 
 exports.getCart = (req, res, next) => {
